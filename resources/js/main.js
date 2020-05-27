c18d6cd31e8903f650f93dfe86ca3cc98702efae
Alex Bobkov <lilalex85@gmail.com>
2020-05-27 20:02:51 +0300

    test

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-    var viewportHeight = $(window).height() - 80 + 'px';
+    var viewportHeight = $(window).height();
     $('.info').text(viewportHeight);
 
+    $('.main-container').scroll(function() {
+        $('.info').text(viewportHeight);
+      });
+
     // $('.button').css('top', 'calc(' + viewportHeight + ' - 80px)');
-    $('#btn').css('top', viewportHeight);
+    $('#btn').css('top', viewportHeight - 80 + 'px');
   });
