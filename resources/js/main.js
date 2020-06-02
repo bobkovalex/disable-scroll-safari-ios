6df5ed5d739eb0d1be441561bb6e8d7ab425b639
Alex Bobkov <lilalex85@gmail.com>
2020-06-02 13:59:02 +0300

    prevent scroll

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,18 +1,30 @@
 $(document).ready(function(){
+    var scrollPositionY = 0;
     
+    // Open Dialog
     $('#btn-open').click(function(){
-        var pos = window.scrollY + window.innerHeight - $('#dialog').height();
+        // Calc current scroll position
+        scrollPositionY = window.scrollY;
+        var pos = scrollPositionY + window.innerHeight - $('#dialog').height();
+        // Set page container position to avoid scrollon to top after aplying position:fixed
+        $('body').css('top', '-' + window.scrollY + 'px');
+        // Prevent container from scrolling
         $('body').css('position', 'fixed');
-        $('html').css('position', 'fixed');
+        // Set dialog position at bottom of the visible screen
         $('#dialog').css('top', pos + 'px');
-        $('#dialog').css('width', window.innerWidth + 'px');
+        $('#overlay').css('z-index', '9');
+        $('#dialog').css('z-index', '999');
+        // Show dialog & overlay
         $('#overlay').show();
         $('#dialog').show();
     });
 
+    // Close Dialog
     $('#btn-close').click(function(){
+        // Unset fixed position & prevent scrolling to top
         $('body').css('position', 'unset');
-        $('html').css('position', 'unset');
+        $('body').scrollTop(scrollPositionY);
+        // Hide dialog & overlay
         $('#overlay').hide();
         $('#dialog').hide();
     });
